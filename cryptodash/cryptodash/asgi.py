070af995a1b3aebb3979ac32c4715a1d16b651fe
graham-arnold-uw <graham.arnold.uw@gmail.com>
2021-03-02 20:02:39 -0800

    cryptodash project started, testing sk hiding

--- cryptodash/cryptodash/asgi.py
+++ cryptodash/cryptodash/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cryptoDash project.
+ASGI config for cryptodash project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptoDash.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptodash.settings')
 
 application = get_asgi_application()
--- cryptodash/cryptodash/wsgi.py
+++ cryptodash/cryptodash/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cryptoDash project.
+WSGI config for cryptodash project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptoDash.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptodash.settings')
 
 application = get_wsgi_application()
