57a806583b4c92e6636bfae1c4fb9c6fda493b66
graham-arnold-uw <graham.arnold.uw@gmail.com>
2021-08-02 20:01:38 -0700

    finished updating to project level template and created a simple dasbhboard homepage that links out to the other apps. Tweaked the css styling of the project template as well

--- cryptodash/cryptodash/settings.py
+++ cryptodash/cryptodash/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pricestream.apps.PricestreamConfig',
+    'dashboard.apps.DashboardConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- cryptodash/dashboard/views.py
+++ cryptodash/dashboard/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+
 def index(request):
+    app_names = ['pricestream']
+    template = loader.get_template('dashboard/home.html')
+    context = {
+        'app_list':app_names
+    }
+    return HttpResponse(template.render(context,request))
+
+def index_simple(request):
     return HttpResponse("Hello this is the dashboard home. App links below")
